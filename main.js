76e07f0e21d23b580093fdc31362e06a73c98e70
MavlonjonMirzaxalilov <mirzaxalilovmavlonjon7@gmail.com>
2025-03-12 14:20:46 +0500

    boshlandi

---
+++ main.js
@@ -0,0 +1,28 @@
+fetch('https://6560acc483aba11d99d151a5.mockapi.io/api/movies')
+	.then(response => response.json())
+	.then(data => {
+		data.forEach(movie => {
+			console.log(movie)
+
+			let box = document.querySelector('.quti')
+			box.innerHTML += `
+			 <div class="row">
+    <div class="col">
+      <div class="card">
+        <div class="card-image">
+          <img src='${movie.movieImg}'/>
+          <span class="card-title">${movie.movieName}</span>
+        </div>
+        <div class="card-content">
+          <h6>${movie.starring}</h6>
+		  <p>${movie.releasedDate}</p>
+        </div>
+        <div class="card-action">
+          <a href=${movie.trailer}>Watch trailer</a>
+        </div>
+      </div>
+    </div>
+  </div>
+			`
+		})
+	})
